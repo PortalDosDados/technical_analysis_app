199e2bee8e0f45971b13c3c3ee0dd4321ddcafdf
Dione Nascimento <morrone.dione@gmail.com>
2025-10-19 17:13:20 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import yfinance as yf
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 import warnings
 warnings.filterwarnings("ignore")
 
 # Definir parâmetros
-tickers = ['BBSE3.SA','BBAS3.SA']
+tickers = ['BBSE3.SA']
 start_date = '2020-01-01'
-end_date = '2023-01-01'
+end_date = '2025-10-19'
 
 # Baixar dados históricos
 df = yf.download(tickers, start=start_date, end=end_date)
@@ -29,7 +30,19 @@ df_long['Id'] = range(1, len(df_long) + 1)
 # Selecionar apenas as colunas desejadas e na ordem certa
 df_long = df_long[['Id', 'Ticker', 'Data', 'Abertura', 'Fechamento', 'Volume']]
 
-df_long.to_csv('dados_historicos.csv', index=False)
+# Salvar em CSV
+#df_long.to_csv('dados_historicos.csv', index=False)
 
 # Exibir os primeiros registros
 #print(df_long.head())
+
+# Plotar os dados de fechamento
+plt.figure(figsize=(12, 6))
+for ticker in tickers:
+    subset = df_long[df_long['Ticker'] == ticker]
+    plt.plot(subset['Data'], subset['Fechamento'], label=ticker)
+plt.title(f'Preços de Fechamento Históricos - {tickers[0]}')
+plt.ylabel('Preço de Fechamento (R$)')
+plt.grid(True)
+plt.legend()
+plt.show()
