4b57e0fea3afcc81316ef709abfd5c6f79737729
Dione Nascimento <morrone.dione@gmail.com>
2025-10-19 15:40:23 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,14 +6,22 @@ import warnings
 warnings.filterwarnings("ignore")
 
 # Definindo o ticker e o período de análise
-ticker = input(f'Entre com o ticker da ação (ex: BBSE.SA):')
+ticker = input(f'Entre com o ticker da ação (ex: BBSE3.SA):')
 date_start = input('Qual a data do início a ser analisado? (ex: YYYY-MM-DD): ')
 date_end = input('Qual a data final? (ex: YYYY-MM-DD): ')
 
 df = yf.download(ticker, start=date_start, end=date_end)
+'''
+df.reset_index(inplace=True)
+
+df_long = df[['Date', 'Close', 'Volume']].copy()
+
+df_long['Ticker'] = ticker
+
+df_long = df_long[['Ticker', 'Date', 'Open', 'Close', 'Volume']]
 
 
+'''
+#plt.figure(figsize=(12, 6))
 
-print(df.head()) 
-
-plt.figure(figsize=(12, 6))
+print(df.head()) # type: ignore
