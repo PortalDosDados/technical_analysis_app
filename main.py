07a050b35b0eb169a87630073e4c477f6bb9d8d1
Dione Nascimento <morrone.dione@gmail.com>
2025-10-19 14:51:20 -0300

    creating the function input

--- main.py
+++ main.py
@@ -4,5 +4,8 @@ import matplotlib.pyplot as plt
 import warnings
 warnings.filterwarnings("ignore")
 
-
+# Function to fetch stock data
+ticker = input(f'Entre com o ticker da ação (ex: BBSE.SA):')
+date_start = input('Qual a data do início a ser analisado? (ex: YYYY-MM-DD): ')
+date_end = input('Qual a data final? (ex: YYYY-MM-DD): ')
 
