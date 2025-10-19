f1bb4e9ec9d77a495719843b2f22b523434e2275
Dione Nascimento <morrone.dione@gmail.com>
2025-10-19 15:31:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-#Import necessary libraries
+#Importando as bibliotecas necessárias
 import yfinance as yf
 import matplotlib.pyplot as plt
 import pandas as pd
 import warnings
 warnings.filterwarnings("ignore")
 
-# Function to fetch stock data
+# Definindo o ticker e o período de análise
 ticker = input(f'Entre com o ticker da ação (ex: BBSE.SA):')
 date_start = input('Qual a data do início a ser analisado? (ex: YYYY-MM-DD): ')
 date_end = input('Qual a data final? (ex: YYYY-MM-DD): ')
 
 df = yf.download(ticker, start=date_start, end=date_end)
 
+
+
 print(df.head()) 
+
+plt.figure(figsize=(12, 6))
