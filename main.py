922d35c0f06496c483ebaa7efeef603461baf60a
Dione Nascimento <morrone.dione@gmail.com>
2025-10-19 16:17:51 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+#Importar as bibliotecas necessárias
+import yfinance as yf
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings("ignore")
+
+# Definir parametros
+ticker = "BBSE3.SA"
+start_date = "2020-01-01"
+end_date = "2023-01-01"
+
+# Baixar dados históricos
+df = yf.download(ticker, start=start_date, end=end_date)
+
+
 
