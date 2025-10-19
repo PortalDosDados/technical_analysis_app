32757a219f16c2bb55714398f54ccc455006cdda
Dione Nascimento <morrone.dione@gmail.com>
2025-10-19 16:12:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -11,17 +11,27 @@ date_start = input('Qual a data do início a ser analisado? (ex: YYYY-MM-DD): ')
 date_end = input('Qual a data final? (ex: YYYY-MM-DD): ')
 
 df = yf.download(ticker, start=date_start, end=date_end)
-'''
-df.reset_index(inplace=True)
 
-df_long = df[['Date', 'Close', 'Volume']].copy()
+# Se tiver MultiIndex, pega o último nível (nomes reais)
+if isinstance(df.columns, pd.MultiIndex):
+    df.columns = df.columns.get_level_values(-1)
 
-df_long['Ticker'] = ticker
+df.reset_index(inplace=True) # type: ignore
 
-df_long = df_long[['Ticker', 'Date', 'Open', 'Close', 'Volume']]
+df['Ticker'] = ticker  # type: ignore
 
+df = df.rename(columns={
+    'Date': 'Data',
+    'Open': 'Abertura',
+    'High': 'Máxima',
+    'Low': 'Mínima',
+    'Close': 'Fechamento',
+    'Adj Close': 'Fechamento Ajustado',
+    'Volume': 'Volume'
+})  
 
-'''
-#plt.figure(figsize=(12, 6))
+# Selecionando apenas as colunas que você quer
+df_long = df[['Ticker', 'Data', 'Abertura', 'Fechamento', 'Volume']]
 
-print(df.head()) # type: ignore
+# Exibindo resultado
+print(df_long.head())
