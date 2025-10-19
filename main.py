dbdd006a2a97509edc4ceb41257f06427a9e7097
Dione Nascimento <morrone.dione@gmail.com>
2025-10-19 16:46:49 -0300

    Summary

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-#Importar as bibliotecas necessárias
 import yfinance as yf
 import matplotlib.pyplot as plt
 import warnings
 warnings.filterwarnings("ignore")
 
-# Definir parametros
-ticker = "BBSE3.SA"
-start_date = "2020-01-01"
-end_date = "2023-01-01"
+# Definir parâmetros
+tickers = ['BBSE3.SA','BBAS3.SA']
+start_date = '2020-01-01'
+end_date = '2023-01-01'
 
 # Baixar dados históricos
-df = yf.download(ticker, start=start_date, end=end_date)
+df = yf.download(tickers, start=start_date, end=end_date)
 
+# Transformar MultiIndex em formato longo (tickers como coluna)
+df_long = df.stack(level=1).reset_index() #type: ignore
 
+# Renomear colunas para português e substituir 'Price' por 'ID'
+df_long = df_long.rename(columns={
+    'level_1': 'Ticker',
+    'Open': 'Abertura',
+    'Close': 'Fechamento',
+    'Volume': 'Volume',
+    'Date': 'Data'
+})
 
+# Substituir a coluna 'Price' por uma coluna 'Id' sequencial
+df_long['Id'] = range(1, len(df_long) + 1)
+
+# Selecionar apenas as colunas desejadas e na ordem certa
+df_long = df_long[['Id', 'Ticker', 'Data', 'Abertura', 'Fechamento', 'Volume']]
+
+df_long.to_csv('dados_historicos.csv', index=False)
+
+# Exibir os primeiros registros
+#print(df_long.head())
